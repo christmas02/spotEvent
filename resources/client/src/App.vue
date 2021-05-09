edbdf75b4b6f85c2b0a2310188e5730131b1a725
Nanguy <marchenrynanguy1@gmail.com>
2021-05-09 22:09:38 +0000

    vue works

--- resources/client/src/App.vue
+++ resources/client/src/App.vue
@@ -4,6 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+      <v-btn
+          elevation="2"
+      >good</v-btn>
     <router-view/>
   </div>
 </template>
