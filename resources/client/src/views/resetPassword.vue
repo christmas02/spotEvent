9cf008a3fcd027faafbc713028d04650f201221f
marckent04 <marchenrynanguy1@gmail.com>
2021-07-21 15:30:51 +0000

    add restPassword page

---
+++ resources/client/src/views/resetPassword.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-card>
+      <h1>Reinitialisation du mot de passe</h1>
+      <h3>Veillez entrez votre adresse E-mail , nous</h3>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped>
+</style>
