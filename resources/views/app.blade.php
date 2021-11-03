86314068fdb9eda580ec273db06a947d4eda367a
alexis-djidonou <alexisdjidonou@gmal.com>
2021-11-03 12:37:35 +0000

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,8 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 
 <body>
