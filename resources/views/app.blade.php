126a9c1f696ee06892c29005f42ae629ede20291
alexis-djidonou <alexisdjidonou@gmal.com>
2021-11-04 10:23:21 +0000

    mise a jour

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <meta name="description" content="Nous sommes une plateforme de mise en relation entre les acteurs de l'univers du mariage et les personnes désireuses de réaliser un mariage.">
+    <meta name="keywords" content="mariage - prestataires - annuaire nuptial digita">
+    <meta name="author" content="spotevent - impactafric.com">
     <title>Spoteventapp</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
 
-    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="icon" type="image/png" href="{{asset('/public/image/favicon.png')}}" />
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-211953716-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-211953716-1');
+    </script>
+
 </head>
+
 
 <body>
     <div id="app"></div>
