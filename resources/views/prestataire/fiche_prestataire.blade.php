41e26f3d0c9fb2534e50a25e61096885c9f40dcb
alexis-djidonou <alexisdjidonou@gmal.com>
2021-11-20 22:40:19 +0000

    mise ajour backend_webpack

--- resources/views/prestataire/fiche_prestataire.blade.php
+++ resources/views/prestataire/fiche_prestataire.blade.php
@@ -239,8 +239,6 @@
                                 </div>
 
 
-
-
                                 <div class="ln_solid"></div>
                                 <div class="item form-group">
                                     <div class="col-md-6 col-sm-6 offset-md-3">
